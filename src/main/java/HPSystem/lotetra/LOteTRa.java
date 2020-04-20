7346c4a5267d2bd6e0bb862a3e671f25637fe36d
ReiRokusanami <reirokusanami.rdh@gmail.com>
2020-04-20 22:49:15 +0900

    setup gradle.

---
+++ src/main/java/HPSystem/lotetra/LOteTRa.java
@@ -0,0 +1,15 @@
+package HPSystem.lotetra;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(LOteTRa.MODID)
+public class LOteTRa {
+    public static final String MODID = "lotetra";
+    private static final Logger logger = LogManager.getLogger();
+
+    public LOteTRa() {
+
+    }
+}
